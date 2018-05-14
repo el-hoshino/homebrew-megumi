baa5713134fa1918be1802cdebf2ad603a7de964
Elvis Shi <shonetesmith@gmail.com>
2018-05-14 12:34:09 +0900

    Update Casks/megumi.rb to 0.1.1

--- Casks/megumi.rb
+++ Casks/megumi.rb
@@ -1,6 +1,6 @@
 cask 'megumi' do
-  version '0.1.0'
-  sha256 'ae2374ce84d6fa77dffa8aad5d2b6e137ef609f3e6ea45d393ed4d6c858de6b6'
+  version '0.1.1'
+  sha256 'cc09685a0db66611804813acb011544c5a8012b92e9147612c69baec2fce6b0b'
 
   url 'https://github.com/el-hoshino/megumi/releases/download/version%2F0.1.0/megumi.zip'
   name 'megumi'
